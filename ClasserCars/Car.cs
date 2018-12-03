55abc04b6e145b2f732307713e92df79a27348a9
saraglavin <hej@saraglavin.se>
2018-12-03 13:42:04 +0100

    La upp nya filer

---
+++ ClasserCars/Car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClasserCars
+{
+    class Car
+    {
+
+        public int Weight { get; set; }
+        public string Color { get; set; }
+
+        //private string color;
+        //private int weight;
+
+        public Car(string c, int w)
+        {
+            Color = c;
+            Weight = w;
+        }
+
+        public Car()
+        {
+            Color = "Default Blue";
+            Weight = 1200;
+
+        }
+
+        //public void SetWeight(int yy)
+        //{
+        //    weight = yy;
+        //}
+        //public int Getweight()
+        //{
+        //    return weight;
+        //}
+
+        //public void SetColor(string xx)
+        //{
+        //    color = xx;
+        //}
+        //public string GetColor()
+        //{
+        //    return color;
+        //}
+
+    }
+}
---
+++ ClasserCars/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClasserCars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Car c1 = new Car("Blå", 200);
+            Car c2 = new Car();
+
+            Console.WriteLine("Ange färg och vikt, separerat med komma: ");
+            string input = Console.ReadLine();
+
+            string[] separated = input.Split(",");
+
+            string x = separated[0];
+            int y = int.Parse(separated[1]);
+
+            c1.Color = x;
+            c1.Weight = y;
+
+
+
+            //Console.Write("Vilken färg på bil 1? ");
+            //string x = Console.ReadLine();
+            //Console.Write("Vilken färg på bil 2? ");
+            //string y = Console.ReadLine();
+
+            // GetColor här gör att man ex. kan inkludera .Trim eller andra saker för att få output bättre. Till skillnad från om man
+            // man använder variabel x. 
+            //c1.SetColor(x.Trim());
+            //c1.SetWeight(y.Trim());
+
+
+            Console.WriteLine($"The color of the first car is {c1.Color} and the weight is {c1.Weight}");
+            Console.WriteLine($"The color of the first car is {c2.Color} and the weight is {c2.Weight}");
+
+        }
+    }
+}
